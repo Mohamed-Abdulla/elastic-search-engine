e65c8508b2853f7aaae288cfafe8239bf56951ec
mohamed-abdulla <abdabdulla78@gmail.com>
2024-09-28 21:45:01 +0530

    prod url added

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,11 +1,11 @@
 import { Client } from "@elastic/elasticsearch";
 
 export const client = new Client({
-  node: "https://localhost:9200",
+  node: process.env.NODE_ENV == "development" ? process.env.ELASTIC_URL! : process.env.PROD_ELASTIC_URL!,
   tls: {
     rejectUnauthorized: false,
   },
   auth: {
-    apiKey: process.env.ELASTIC_API_KEY!,
+    apiKey: process.env.NODE_ENV == "development" ? process.env.ELASTIC_API_KEY! : process.env.PROD_ELASTIC_API_KEY!,
   },
 });
